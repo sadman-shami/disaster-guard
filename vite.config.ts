bf65c2546b07ac73a2e6558c3b4633a2bc6e4693
Sadman <srahyme413@gmail.com>
2026-08-28 23:58:45 +0600

    lint: fix linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from "vite";
-import { devtools } from "@tanstack/devtools-vite";
-
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
-import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
-	resolve: { tsconfigPaths: true },
-	plugins: [devtools(), nitro(), tailwindcss(), tanstackStart(), viteReact()],
-	server: {
-		port: 3000,
-		host: true,
-	},
+  resolve: { tsconfigPaths: true },
+  plugins: [devtools(), nitro(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
 
 export default config;
